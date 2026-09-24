fcb3828d03623348d289632776144641da66d8a7
Ziliya <ziliya@example.com>
2026-09-24 14:39:00 +0300

    data: свои демо-посты и комментарии

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,16 @@
 -- Demo data (used on application startup, NOT in tests)
 INSERT INTO posts (id, title, text, likes_count) VALUES
-    (1, 'Первый пост', 'Добро пожаловать в блог! Это демонстрационный пост.', 5),
-    (2, 'Markdown', '# Заголовок
-
-Пример текста поста в формате **Markdown**.', 1);
+    (1, 'Первый пост', 'Добро пожаловать в блог!', 5),
+    (2, 'Канье Уест все таки приедет в Питер', 'но это не точно', 1);
 
 ALTER TABLE posts ALTER COLUMN id RESTART WITH 3;
 
 INSERT INTO post_tags (post_id, tag) VALUES
-    (1, 'tag_1'),
-    (1, 'tag_2');
+    (2, 'новости'),
+    (1, 'мем');
 
 INSERT INTO comments (id, post_id, text) VALUES
-    (1, 1, 'Первый комментарий к посту 1'),
-    (2, 2, 'Комментарий к посту 2');
+    (1, 1, 'Очень смешно! хахах'),
+    (2, 2, 'не верю');
 
 ALTER TABLE comments ALTER COLUMN id RESTART WITH 3;
